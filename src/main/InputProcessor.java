8ce63e5fbef2f7f6378f22f0172937a9cbbc98ef
Ana Han <anahan2011@gmail.com>
2017-10-21 18:31:35 -0400

    Commit

--- src/main/InputProcessor.java
+++ src/main/InputProcessor.java
@@ -1,5 +1,27 @@
 package main;
-
+import java.util.ArrayList;
+import net.WebReader;
+/**
+ * @author admin
+ * Gets ingredients from a given website.
+ *
+ */
 public class InputProcessor {
-
+	
+	String url;
+	ArrayList<Ingredient> ingredient;
+	
+	public InputProcessor()
+	{
+		url = "";
+		ingredient = null;
+	}
+	public InputProcessor(String url)
+	{
+		if(url != null) 
+		{
+			ingredient = WebReader.getIngredients(url);
+		} // end of if url is valid
+	} // end of public InputProcessor(String url)
+	
 }
