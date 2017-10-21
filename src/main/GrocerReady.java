70a7d3e201a0ba67ed22629ed27d7c41c61c136f
CardistryMagic <thediamondtorchmc@gmail.com>
2017-10-21 18:06:00 -0400

    Change in main files

--- src/main/GrocerReady.java
+++ src/main/GrocerReady.java
@@ -1,5 +1,18 @@
 package main;
 
+import api.GUI;
+
 public class GrocerReady {
 
+	public static void main(String[] args) {
+
+		GUI GrocerShoppingGUI;
+		GrocerShoppingGUI= new GUI();
+		
+		String[] emptyStringArray = new String[0];
+		
+		GrocerShoppingGUI.init(emptyStringArray);
+
+	}
+	
 }
--- src/net/main.java
+++ src/net/main.java
@@ -1,18 +1,6 @@
 package net;
 
-import javafx.application.Application;
-import api.GUI;
-
 public class main {
 	
-	public static void main(String[] args) {
-
-		GUI GrocerShoppingGUI;
-		GrocerShoppingGUI= new GUI();
-		
-		String[] emptyStringArray = new String[0];
-		
-		GrocerShoppingGUI.init(emptyStringArray);
-
-	}
+	
 }
