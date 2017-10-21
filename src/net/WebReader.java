7e1461ec6620f452ba01c2de473444558ebb24d1
Michael Shi <1027michael@gmail.com>
2017-10-21 17:40:13 -0400

    WebReader setup

---
+++ src/net/WebReader.java
@@ -0,0 +1,28 @@
+package net;
+import java.net.*;
+import java.util.*;
+import main.Ingredient;
+
+import java.io.*;
+
+public class WebReader {
+
+	public static ArrayList<Ingredient> getIngredients(String link) throws IOException{
+		URL url = new URL(link);
+		URLConnection website = url.openConnection();
+		BufferedReader html = new BufferedReader(new InputStreamReader(website.getInputStream()));
+		
+		if(link.length() < 22){ //too short, prevents errors
+			return null;
+		}
+		if(link.substring(0, 23).equals("allrecipes.com")){
+			return fetchAR(html);
+		}
+		
+		return null;
+	}
+	
+	private static ArrayList<Ingredient> fetchAR(BufferedReader html){ //Allrecipes
+		
+	}
+}
