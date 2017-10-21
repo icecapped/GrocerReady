87533cf2c9b987f26cd592a47e3d612c6ef5a15b
Lokeesan Kaneshwaran <LokeesanK@users.noreply.github.com>
2017-10-21 17:44:32 -0400

    merge

--- src/api/GUI.java
+++ src/api/GUI.java
@@ -6,6 +6,24 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class GUI {
+public class GUI extends Application {
+	Button button;
 
+	
+	public void hi(String[] args) {
+        launch(args);
+    }
+	
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the Window");
+        button = new Button();
+        button.setText("Click me");
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
 }
