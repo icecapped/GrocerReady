8140644a686271bd662d22443190a88f57587d98
CardistryMagic <thediamondtorchmc@gmail.com>
2017-10-22 09:02:41 -0400

    GUI Update

--- src/api/GUI.java
+++ src/api/GUI.java
@@ -7,21 +7,13 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import java.util.ArrayList;
 import javafx.scene.text.*;
-import javafx.stage.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.control.*;
-import javafx.scene.*;
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-//import javafx.scene.effect.BlendMode;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-//import java.lang.Object.*;
 
 public class GUI extends Application {
 
@@ -34,7 +26,7 @@ public class GUI extends Application {
 	Button submitURLs;
 	Text recipeInputTitle;
 	BorderPane ingredientBorderPane;
-	HBox titleAndURL;
+	HBox recipeInputTitleAndURL;
 	Scene scene;
 	TextField URLField;
 	TextArea URLDisplay;
@@ -45,29 +37,27 @@ public class GUI extends Application {
 	BorderPane ingredientsBorderPane;
 	HBox ingredientsTitleAndURL;
 	TextArea ingredientsDisplay;
-	Stage ingredientStage;
-	Scene finalScene;
+	Stage ingredientsStage;
+	Scene ingredientsScene;
 	
     @Override
     public void start(Stage primaryStage) throws Exception {
-    	
     	
     	//URL Field
     	URLField = new TextField();
     	URLField.setText("Enter Recipe URL here...");
     	URLField.setPrefSize(650, 60);
     	URLField.setFont(Font.font("Segoe IU Light", FontWeight.LIGHT, 30));
-    	//GridPane for top margin for URL Field
+    	//Grid Pane for URL Field
     	GridPane URLFieldGrid = new GridPane();
     	URLFieldGrid.setPadding(new Insets(25,0,0,0));
     	URLFieldGrid.add(URLField, 0, 0);
     	
-    	//addURL Button
+    	//Add URL Button
     	addURL = new Button();
     	addURL.setText("Add URL");
     	addURL.setPrefSize(90, 60);
-//    	addURL.setStyle("fx-background-color: #000000");
-    	//GridPane for top margin for addURL Button
+    	//Grid Pane for Add URL Button
     	GridPane addURLGrid = new GridPane();
     	addURLGrid.setPadding(new Insets(25,0,0,0));
     	addURLGrid.add(addURL, 0, 0);
@@ -89,7 +79,6 @@ public class GUI extends Application {
                 if (errorOrNo) {
                 	//Update URL String/Log
                 	URLs = URLs + "Invalid URL.\n";
-                	//remember to filter out "Invalid URL.\n" before passing on
                 }
                 else {
                 	URLs = URLs + URLField.getText() + "\n";
@@ -123,12 +112,8 @@ public class GUI extends Application {
             	}
             	
             	String[] finalOutput = InputProcessor.getIngredients(totalURLs);
-            	//Test output
-//            	for (int i = 0; i < finalOutput.length; i++) {
-//            		System.out.println(finalOutput[i]);
-//            	}
             	         	
-            	ingredientStage = new Stage();
+            	ingredientsStage = new Stage();
             	ingredientsBorderPane = new BorderPane();
             	
                 
@@ -136,14 +121,13 @@ public class GUI extends Application {
                 ingredientsTitleAndURL = new HBox();
             	
                 //set window title
-            	ingredientStage.setTitle("Grocer Ready - Ingredients List");     
+            	ingredientsStage.setTitle("Grocer Ready - Ingredients List");     
                 
             	//main title
             	ingredientsTitle = new Text("Grocer Ready - Ingredients List");
             	ingredientsTitle.setFont(Font.font("Segoe UI Light", FontWeight.BOLD, 75));
             	ingredientsTitle.setFill(Color.WHITE);
-//            	ingredientsTitle.setOpacity(0);
-                
+            	
                 //initialize horizontal box
                 ingredientsTitleAndURL.getChildren().add(ingredientsTitle);
                 ingredientsTitleAndURL.setPadding(new Insets(0, 10, 10, 20));
@@ -162,7 +146,8 @@ public class GUI extends Application {
                 }
                 
                 ingredientsDisplay.setText(finalDisplayOutput);
-                //grid to add padding to text box
+                
+                //Grid to add padding to text box
                 GridPane ingredientsDisplayGrid = new GridPane();
                 ingredientsDisplayGrid.setPadding(new Insets(30,35,30,35));
                 ingredientsDisplayGrid.add(ingredientsDisplay, 0, 0);
@@ -170,15 +155,15 @@ public class GUI extends Application {
                 ingredientsBorderPane.setTop(ingredientsTitleAndURL);
                 ingredientsBorderPane.setCenter(ingredientsDisplayGrid);
                 
-                finalScene = new Scene(ingredientsBorderPane, 1280, 720);
-                ingredientStage.setScene(finalScene);
-                ingredientStage.show();
+                ingredientsScene = new Scene(ingredientsBorderPane, 1280, 720);
+                ingredientsStage.setScene(ingredientsScene);
+                ingredientsStage.show();
                 
             }
         });
     	
     	//horizontal box for title, URL field and addURL button
-        titleAndURL = new HBox();
+        recipeInputTitleAndURL = new HBox();
     	
         //set window title
     	primaryStage.setTitle("Grocer Ready - Recipe Input");     
@@ -190,12 +175,12 @@ public class GUI extends Application {
     	recipeInputTitle.setOpacity(0.9);
         
         //initialize horizontal box
-        titleAndURL.getChildren().add(recipeInputTitle);
-        titleAndURL.getChildren().add(URLFieldGrid);
-        titleAndURL.getChildren().add(addURLGrid);
-        titleAndURL.setPadding(new Insets(0, 10, 10, 20));
-        titleAndURL.setSpacing(30);
-        titleAndURL.setStyle("-fx-background-color: #72A329;");
+        recipeInputTitleAndURL.getChildren().add(recipeInputTitle);
+        recipeInputTitleAndURL.getChildren().add(URLFieldGrid);
+        recipeInputTitleAndURL.getChildren().add(addURLGrid);
+        recipeInputTitleAndURL.setPadding(new Insets(0, 10, 10, 20));
+        recipeInputTitleAndURL.setSpacing(30);
+        recipeInputTitleAndURL.setStyle("-fx-background-color: #72A329;");
         
         //bottom text box
         URLDisplay = new TextArea();
@@ -211,7 +196,7 @@ public class GUI extends Application {
         
         //main body ingredientBorderPane pane
         ingredientBorderPane = new BorderPane();
-        ingredientBorderPane.setTop(titleAndURL);
+        ingredientBorderPane.setTop(recipeInputTitleAndURL);
         ingredientBorderPane.setCenter(URLListGrid);
         ingredientBorderPane.setBottom(submitURLsGrid);
         
