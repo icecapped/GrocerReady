d331b45eea8a6e8a1d642a709d7faf09631a78bd
Michael Shi <1027michael@gmail.com>
2017-10-21 21:57:02 -0400

    Fixed WebReader

--- src/net/WebReader.java
+++ src/net/WebReader.java
@@ -19,24 +19,25 @@ public class WebReader {
 		if(link.length() < 22){ //too short, prevents errors
 			return null;
 		}
-		if(link.substring(0, 23).equals("allrecipes.com")){
+		if(link.substring(0, 29).equals("http://allrecipes.com/recipe/")){
 			return fetchAR(html);
 		}
 		
-		return null;
+			return null;
 	}
 	
 	private static ArrayList<String> fetchAR(BufferedReader html) throws IOException{ //Allrecipes
 		ArrayList<Ingredient> list = new ArrayList<Ingredient>();
-		
+
 		String s;
 		while((s = html.readLine()) != null){
 			if(s.contains("itemprop=\"ingredients\"")){ // fetching ingredients
 				String ingredientString;
 				int left = s.indexOf("itemprop=\"ingredients\"");
-				int right = s.substring(left).indexOf("<");
-				
-				ingredientString = s.substring(left, right);
+				String temp = s.substring(left);
+				int right =	temp.indexOf('<');
+				ingredientString = temp.substring(23, right);
+				System.out.println(ingredientString);
 				list.add(new Ingredient());
 			}
 		}
