93a7791477b4bc962f1c145689ab1d9dcbcc3809
Ana Han <anahan2011@gmail.com>
2017-10-21 20:37:10 -0400

    InputProcessor Update

--- src/main/InputProcessor.java
+++ src/main/InputProcessor.java
@@ -1,4 +1,5 @@
 package main;
+import java.io.IOException;
 import java.util.ArrayList;
 import net.WebReader;
 /**
@@ -9,26 +10,58 @@ import net.WebReader;
 public class InputProcessor {
 	
 	String url;
-	ArrayList<Ingredient> ingredient;
+	
 	
 	public InputProcessor()
 	{
 		url = "";
-		ingredient = null;
+		
 	}
-	public InputProcessor(String url)
+	/**
+	 * Gets an ingredient list from the GUI and formats then returns it as an array
+	 * @param url
+	 * @return the ingredients represented by strings
+	 */
+	public String[] getIngredients(ArrayList<String> url)
 	{
+		ArrayList<Ingredient> ingredient;
 		if(url != null) 
 		{
 			try 
 			{
-				ingredient = WebReader.getIngredients(url);
+				for(int i = 0; i < url.size() - 1; i++)
+				{
+					ingredient.add(WebReader.getIngredients(url[i]));
+					// add together ingredients from different recipes.
+				} // end of for(int i = 0; i < url.size() - 1; i++)
 			}
 			catch (IOException e)
 			{
-				// 
+				
+				return new String[] {"ERROR (2)"};
 			}
+			// consolidate the ingredients.
+			ingredient = IngredientCollector.processedList(ingredient);
+			// convert the ingredients into a array of string ingredient representations
+			String[] finalIngredient = new String[ingredient.size()];
+			for (int i = 0; i < finalIngredient.length - 1; i++)
+			{
+				finalIngredient[i] = ingredient.get(i).toString();
+			} // end of for for (int i = 0; i < finalIngredient.length - 1, i++)
+			
+			// return an array of string ingredient representations.
+			return finalIngredient;
+			 
 		} // end of if url is valid
-	} // end of public InputProcessor(String url)
+		else
+		{
+			String[] finalIngredient = new String[1];
+			finalIngredient[0] = "Please enter a valid url.";
+			return finalIngredient;
+		}
+	}
 	
-}
+	
+} // end of public InputProcessor(String url)
+	
+
